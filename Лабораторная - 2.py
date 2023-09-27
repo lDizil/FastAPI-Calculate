ff4a535f7e11489b4dd33be3ec342444211ebde5
Denis <48453680+lDizil@users.noreply.github.com>
2023-09-27 11:48:37 +0300

    Update Лабораторная - 2.py

--- Лабораторная - 2.py
+++ Лабораторная - 2.py
@@ -13,17 +13,16 @@ def calculator(first_number: int, second_number: int, operator: str):
     if operator == "*":
         result = first_number * second_number
         return result
-    if operator == "**" and second_number != 0:
+    if operator == "**":
         result = first_number ** second_number
         return result
     if operator == "/" and second_number != 0:
         result = first_number / second_number
         return result
+    if operator == "/" and second_number == 0:
+        return "Unreal"
     else:
-        if operator == "^" and second_number == 0:
-            return 1
-        if operator == "/" and second_number == 0:
-            return "Unreal"
+        return "Unreal"
 
 
 
